5857340f8a44dcfab6b1d60864196cccfa37dd15
Kapitany777 <kapitany777@gmail.com>
2026-01-20 20:11:18 +0100

    The liderckek.php page is now HTML 5 instead of XHTML

--- liderckek.php
+++ liderckek.php
@@ -1,36 +1,37 @@
 <?php
-include_once("include/inc_page.php");
+include_once "include/inc_page.php";
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html lang="hu">
 
 <head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" href="felulet/liderckek.css" type="text/css" />
-<title>Lidérckék - Képregény</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="felulet/liderckek.css" type="text/css">
+    <title>Lidérckék - Képregény</title>
 
-<?php
-function Lapozo($kep_id)
-{
-print("<p style=\"text-align: center;\">\n");
+    <?php
+    function Lapozo($kep_id)
+    {
+        print("<p style=\"text-align: center;\">\n");
 
-print("<a href=\"liderckek.php?kep_id=1\">");
-print("<img src=\"felulet/elso.jpg\" title=\"Első rész\" alt=\"Első rész\" border=\"0\" hspace=\"15\" vspace=\"10\" />");
-print("</a>\n");
-print("<a href=\"liderckek.php?kep_id=".($kep_id > 1 ? $kep_id - 1 : LIDERCKEK_MAX)."\">");
-print("<img src=\"felulet/elozo.jpg\" title=\"Előző rész\" alt=\"Előző rész\" border=\"0\" hspace=\"15\" vspace=\"10\" />");
-print("</a>\n");
-print("<a href=\"liderckek.php?kep_id=".($kep_id < LIDERCKEK_MAX ? $kep_id + 1 : 1)."\">");
-print("<img src=\"felulet/kovetkezo.jpg\" title=\"Következő rész\" alt=\"Következő rész\" border=\"0\" hspace=\"15\" vspace=\"10\" />");
-print("</a>\n");
-print("<a href=\"liderckek.php?kep_id=".LIDERCKEK_MAX."\">");
-print("<img src=\"felulet/utolso.jpg\" title=\"Utolsó rész\" alt=\"Utolsó rész\" border=\"0\" hspace=\"15\" vspace=\"10\" />");
-print("</a>\n");
-print("</p>\n");
-}
-?>
+        print("<a href=\"liderckek.php?kep_id=1\">");
+        print("<img src=\"felulet/elso.jpg\" title=\"Első rész\" alt=\"Első rész\">");
+        print("</a>\n");
+        print("<a href=\"liderckek.php?kep_id=" . ($kep_id > 1 ? $kep_id - 1 : LIDERCKEK_MAX) . "\">");
+        print("<img src=\"felulet/elozo.jpg\" title=\"Előző rész\" alt=\"Előző rész\">");
+        print("</a>\n");
+        print("<a href=\"liderckek.php?kep_id=" . ($kep_id < LIDERCKEK_MAX ? $kep_id + 1 : 1) . "\">");
+        print("<img src=\"felulet/kovetkezo.jpg\" title=\"Következő rész\" alt=\"Következő rész\">");
+        print("</a>\n");
+        print("<a href=\"liderckek.php?kep_id=" . LIDERCKEK_MAX . "\">");
+        print("<img src=\"felulet/utolso.jpg\" title=\"Utolsó rész\" alt=\"Utolsó rész\">");
+        print("</a>\n");
+        print("</p>\n");
+    }
+
+    ?>
 </head>
 
 <body>
@@ -40,42 +41,41 @@ include("liderckek_max.php");
 
 if (isset($_GET["kep_id"]))
 {
-  $kep_id = (integer)$_GET["kep_id"];
-  
-  if ($kep_id<1 || $kep_id>LIDERCKEK_MAX) $kep_id = 1;
-}
+    $kep_id = (integer)$_GET["kep_id"];
 
+    if ($kep_id < 1 || $kep_id > LIDERCKEK_MAX) $kep_id = 1;
+}
 else
 {
-  $kep_id = 1;
+    $kep_id = 1;
 }
 ?>
 
 <div id="container">
 
-<?php
-Menu();
-?>
+    <?php
+    Menu();
+    ?>
 
-<div id="txt">
-<h1>Lidérckék képregény</h1>
+    <div id="txt">
+        <h1>Lidérckék képregény</h1>
 
-<?php
-Lapozo($kep_id);
-?>
+        <?php
+        Lapozo($kep_id);
+        ?>
 
-<p style="text-align: center;">
-<?php print("<img src=\"liderckek/liderckek".$kep_id.".jpg\" border=\"0\" alt=\"Lidérckék\" />\n"); ?>
-</p>
+        <p class="comics">
+            <?php print("<img src=\"liderckek/liderckek" . $kep_id . ".jpg\" alt=\"Lidérckék\">\n"); ?>
+        </p>
 
-<?php
-Lapozo($kep_id);
-?>
-</div>
+        <?php
+        Lapozo($kep_id);
+        ?>
+    </div>
 
-<?php
-Footer();
-?>
+    <?php
+    Footer();
+    ?>
 
 </div>
 
