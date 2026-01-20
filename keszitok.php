2ac703207cf65b9d86d2cb02221af70e0745e13d
Kapitany777 <kapitany777@gmail.com>
2026-01-20 19:55:06 +0100

    The keszitok.php page is now HTML 5 instead of XHTML

--- keszitok.php
+++ keszitok.php
@@ -1,58 +1,53 @@
 <?php
-include_once("include/inc_page.php");
+include_once "include/inc_page.php";
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html lang="hu">
 
 <head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" href="felulet/liderckek.css" type="text/css" />
-<title>Lidérckék - Készítők</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="felulet/liderckek.css" type="text/css">
+    <title>Lidérckék - Készítők</title>
 </head>
 
 <body>
 
 <div id="container">
 
-<?php
-Menu();
-?>
+    <?php
+    Menu();
+    ?>
 
-<div id="txt">
-<h1>Az oldal készítői</h1>
+    <div id="txt">
+        <h1>Az oldal készítői</h1>
 
-<p>
-Grafikai megvalósítás:
-</p>
+        <p>
+            Grafikai megvalósítás:
+        </p>
 
-<ul>
-<li>Lidérckék figurák, egyéb tárgyak, képsorozatok: Jimmy Cartwright</li>
-<li>Háttér: Dismal Swamp Monte Carolina</li>
-</ul>
+        <ul>
+            <li>Lidérckék figurák, egyéb tárgyak, képsorozatok: Jimmy Cartwright</li>
+            <li>Háttér: Dismal Swamp Monte Carolina</li>
+        </ul>
 
-<p>
-Szöveg és grafikai ötleteket adtak:
-</p>
+        <p>
+            Szöveg és grafikai ötleteket adtak:
+        </p>
 
-<ul>
-<li>Ida</li>
-<li>Kapitány</li>
-<li>Anonymus Chynewa</li>
-<li>Tóth Ferenc</li>
-<li>Homoergaster</li>
-</ul>
+        <ul>
+            <li>Ida</li>
+            <li>Kapitány</li>
+            <li>Anonymus Chynewa</li>
+            <li>Tóth Ferenc</li>
+            <li>Homoergaster</li>
+        </ul>
+    </div>
 
-<p>
-Elérhetőségünk: <a href="mailto:lidercfeny@szentesinfo.hu">lidercfeny@szentesinfo.hu</a>
-</p>
-
-</div>
-
-<?php
-Footer();
-?>
+    <?php
+    Footer();
+    ?>
 
 </div>
 
